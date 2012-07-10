2ce7003891fb37ccf8b4f76ea6eacf862389cc47
Arjan Singh <arjansingh1989@gmail.com>
2012-07-10 14:57:47 -0400

    Incorporated Lisa's x-li tag into our scrollable list view.

---
+++ newelements/listitem/listitem.js
@@ -0,0 +1,21 @@
+//Credit for the x-li tag goes to Lisa Neigut.
+
+(function(){
+
+	var openLink = function (e) {
+		window.location = this.getAttribute('href');	
+	};
+	xtag.register('x-li', {
+		events: {
+			'tap': openLink
+		},
+		onCreate : function(){
+			var href = this.getAttribute('href');
+			var a = document.createElement('a');
+			this.style.cursor = 'pointer';
+			a.setAttribute('href', href);
+			a.style.display='none';
+			this.appendChild(a);
+		}
+	});
+})();
