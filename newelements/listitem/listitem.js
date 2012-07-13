b7be78404663cd236cc07b18c3157978bde161a4
Arjan Singh <arjansingh1989@gmail.com>
2012-07-13 15:36:36 -0400

    Moving the cursor now disengages the link

--- newelements/listitem/listitem.js
+++ newelements/listitem/listitem.js
@@ -2,20 +2,24 @@
 
 (function(){
 
-	var openLink = function (e) {
-		window.location = this.getAttribute('href');	
+	var openLink = function (e,that) {
+		window.location = that.getAttribute('href');	
 	};
         var g_yPosBegin;
         var yPositionBeginClick = function(e){
 	    g_yPosBegin = e.screenY;
 	};
         var yPositionEndClick = function(e){
-	    var offset = 30;
+	    var offset = 30;	
+	    var that = this;
 	    var biggerthan = (g_yPosBegin <= e.screenY + offset);
 	    var lessthan = (g_yPosBegin >= e.screenY - offset);
-	    if(biggerthan || lessthan ){
-		openLink;
+	    if(biggerthan && lessthan ){ 
+		openLink(e,that);
+		console.log(biggerthan);
+		console.log(lessthan);
 	    }
+	    /*openLink(e,that);*/
 	};
 	xtag.register('x-li', {
 		events: {
