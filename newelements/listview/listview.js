b7653f8cdecd4238d47d222f8264f935d6f837fe
Arjan Singh <arjansingh1989@gmail.com>
2012-07-14 11:31:53 -0400

    Updated demo.html and removed x-li tag

--- newelements/listview/listview.js
+++ newelements/listview/listview.js
@@ -1,2 +1,24 @@
-xtag.register('x-listview', {
-});
+(function(){
+	var openLink = function (e,that) {
+	    window.location = that.getAttribute('href');	
+	};
+        var g_yPosBegin;
+        var yPositionBeginClick = function(e){
+	    g_yPosBegin = e.screenY;
+	};
+        var yPositionEndClick = function(e){
+	    var offset = 30;	
+	    var that = this;
+	    if((g_yPosBegin <= e.screenY + offset) && (g_yPosBegin >= e.screenY - offset)){ 
+		openLink(e,that);
+	    }
+	};
+        xtag.register('x-listview', {
+        });
+	xtag.query('li a', {
+	    events: {
+		'mousedown': yPositionBeginClick,
+		'mouseup': yPositionEndClick
+	    }
+	});
+})();
