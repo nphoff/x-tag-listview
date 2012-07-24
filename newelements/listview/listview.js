485013fb39511bb59c3e6092c19f5ca8da27f53a
Arjan Singh <arjansingh1989@gmail.com>
2012-07-24 18:53:00 -0400

    removed unnecessary css

--- newelements/listview/listview.js
+++ newelements/listview/listview.js
@@ -1,24 +1,25 @@
 (function(){
-	var openLink = function (e,that) {
-	    window.location = that.getAttribute('href');	
-	};
-        var g_yPosBegin;
-        var yPositionBeginClick = function(e){
-	    g_yPosBegin = e.screenY;
-	};
-        var yPositionEndClick = function(e){
-	    var offset = 30;	
-	    var that = this;
-	    if((g_yPosBegin <= e.screenY + offset) && (g_yPosBegin >= e.screenY - offset)){ 
-		openLink(e,that);
-	    }
-	};
+
         xtag.register('x-listview', {
         });
-	xtag.query('li a', {
-	    events: {
-		'mousedown': yPositionBeginClick,
-		'mouseup': yPositionEndClick
-	    }
-	});
+
+        //The following code prevents a link from firing when touch scrolling, 
+        //by tracking the vertical position of a click.
+        var g_yPosBegin;
+        var yPositionBeginClick = function(e){
+            g_yPosBegin = e.screenY;
+        };
+        var yPositionEndClick = function(e){
+            var offset = 30;	
+            var that = this;
+            if((g_yPosBegin <= e.screenY + offset) && (g_yPosBegin >= e.screenY - offset)){ 
+                window.location = that.getAttribute('href');	
+            }
+        };
+        xtag.query(this, 'li a', {
+            events: {
+                'mousedown': yPositionBeginClick,
+                'mouseup': yPositionEndClick
+            }
+        });
 })();
